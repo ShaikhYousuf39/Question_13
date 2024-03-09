0558fc752476fa9578e0f0e4ce0b837702a2e3d6
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-03-10 01:10:06 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,2 @@
+var transportations = ["Bugatti", "Audi", "Lamborghini"];
+transportations.map(function (items) { return console.log("I would like to buy a ".concat(items)); });
---
+++ new.ts
@@ -0,0 +1,3 @@
+let transportations: string[] = ["Bugatti","Audi","Lamborghini"];
+
+transportations.map((items) => console.log(`I would like to buy a ${items}`));
